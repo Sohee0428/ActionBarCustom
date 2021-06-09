5f9408027104e467884590222d4a8968eb81b540
Sohee <sohee3480@gmail.com>
2021-06-09 21:02:36 +0900

    액션바에 커즌을 추가 -> 액티비티로 가져오기 -> 토스트 등 이벤트 붙여보기

--- app/src/main/java/com/example/actionbarcustom/MainActivity.kt
+++ app/src/main/java/com/example/actionbarcustom/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.actionbarcustom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+//    액션바에 있는 UI를 담아둘 변수
+//    변수 : 멤버 변수 => 답아주는 행위 : onCreate
+
+    lateinit var toastBnt : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +30,13 @@ class MainActivity : AppCompatActivity() {
 //        커스텀뷰를 들고있는 튤바를 변수로 저장
         val toolbar = defaultActionBar.customView.parent as Toolbar
         toolbar.setContentInsetsAbsolute(0, 0)
+        
+//        커스텀 액션바에 달아둔 버튼을 찾아오기
+        toastBnt = defaultActionBar.customView.findViewById(R.id.toastBtn)
+        
+        toastBnt.setOnClickListener {
+            Toast.makeText(this, "액션바를 통한 토스트", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }
